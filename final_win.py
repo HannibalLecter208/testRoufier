6d0fddc5178aee69be74d4006cec94323020266c
HannibalLecter208 <Hannibal.2025@yandex.ru>
2026-04-26 13:23:49 +0300

    add files

---
+++ final_win.py
@@ -0,0 +1,2 @@
+# напиши здесь код третьего экрана приложения
+C:\Program Files\Algoritmika\vscode\data\extensions\algovscode.algoritmika-python-20250904.95201.0\data\student\70490777\546853\final_win.py
